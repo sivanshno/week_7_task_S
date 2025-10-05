d31e7116c88956bd935e53f9c6f170a504eaf278
sivanshno <e1486332@u.nus.edu>
2025-10-05 23:58:23 +0800

    Add top module

--- FDP.srcs/sources_1/new/Top_Student.v
+++ FDP.srcs/sources_1/new/Top_Student.v
@@ -25,16 +25,14 @@ wire frame_begin;
 wire sending_pixels;
 wire sample_pixel;
 wire [12:0] pixel_index;
-reg [15:0] oled_data = 16'h07E0;
+wire[15:0] oled_data;
 assign JC[2] = 0;
 
 wire CLK6P25;
 wire CLK25;
-wire CLK50Hz;
 
 clk6p25m oledClk( .Clock (clk), .signalOut(CLK6P25) );
 clk25m refreshClk(  .Clock (clk), .signalOut(CLK25) );
-fiftyHzGenerator movementClock ( .Clock(clk), .signalOut( CLK50Hz));
 
 wire [6:0] x;
 wire [5:0] y;
@@ -45,37 +43,15 @@ assign y = pixel_index / 96;
     Oled_Display(.clk(CLK6P25), .reset(0), .frame_begin(frame_begin), 
     .sending_pixels(sending_pixels), .sample_pixel(sample_pixel), .pixel_index(pixel_index),
      .pixel_data( oled_data ), .cs(JC[0]), .sdin(JC[1]), .sclk(JC[3]), .d_cn(JC[4]), .resn(JC[5]), .vccen(JC[6]),
-     .pmoden(JC[7])  );
+     .pmoden(JC[7])  );   
      
-reg [6:0] circle_x;
-reg [5:0] circle_y;
-wire [6:0] x_mover;
-wire [5:0] y_mover;
-
-always @ ( posedge CLK25) begin
-    circle_x <= x_mover;
-    circle_y <= y_mover;
-end
-
-moveCircle circleMover( .clk50hz(CLK50Hz), .clk25mhz(CLK25), .btnU( btnU), .btnD( btnD), .btnR( btnR), .btnL( btnL), .x_out(x_mover), .y_out(y_mover));
-    
- wire numberFrame;
- wire number;
- wire isCircle;
- isNumberFrame determineNumberFrame( .x(x), .y(y), .signalOut( numberFrame));
- isNumber determineNumber( .x(x), .y(y), .signalOut(number));
- circle findCircle( .x(x), .y(y), .circle_x(circle_x), .circle_y(circle_y), .signalOut(isCircle));
- 
-    always @ (posedge CLK25) begin
-        if ( isCircle ) begin
-            oled_data = 16'hf800;
-        end else if ( numberFrame ) begin
-             oled_data = 16'hffff;
-        end else if (number) begin
-            oled_data = 16'h07e0;
-        end else begin
-            oled_data = 16'h0000;
-        end
-    end
-
-endmodule
+taskS dut(
+         .btnU (btnU),
+         .btnR(btnR),
+         .btnL(btnL),
+         .btnD(btnD),
+         .clk(clk),
+         .x(x),
+         .y(y),
+         .oled_data(oled_data)
+         );endmodule
--- FDP.srcs/sources_1/new/taskS.v
+++ FDP.srcs/sources_1/new/taskS.v
@@ -24,6 +24,50 @@ module taskS(
     input btnU,
     input btnR,
     input btnL,
-    input btnD
+    input btnD,
+    input clk,
+    input [6:0] x,
+    input [5:0] y,
+    output reg [15:0] oled_data
     );
+    
+    wire CLK25;
+    wire CLK50Hz;
+    
+    clk25m refreshClk(  .Clock (clk), .signalOut(CLK25) );
+    fiftyHzGenerator movementClock ( .Clock(clk), .signalOut( CLK50Hz));
+    
+         
+reg [6:0] circle_x;
+reg [5:0] circle_y;
+wire [6:0] x_mover;
+wire [5:0] y_mover;
+
+always @ ( posedge CLK25) begin
+   circle_x <= x_mover;
+   circle_y <= y_mover;
+end
+
+moveCircle circleMover( .clk50hz(CLK50Hz), .clk25mhz(CLK25), .btnU( btnU), .btnD( btnD), .btnR( btnR), .btnL( btnL), .x_out(x_mover), .y_out(y_mover));
+   
+wire numberFrame;
+wire number;
+wire isCircle;
+isNumberFrame determineNumberFrame( .x(x), .y(y), .signalOut( numberFrame));
+isNumber determineNumber( .x(x), .y(y), .signalOut(number));
+circle findCircle( .x(x), .y(y), .circle_x(circle_x), 
+.circle_y(circle_y), .signalOut(isCircle));
+
+   always @ (posedge CLK25) begin
+       if ( isCircle ) begin
+           oled_data = 16'hf800;
+       end else if ( numberFrame ) begin
+            oled_data = 16'hffff;
+       end else if (number) begin
+           oled_data = 16'h07e0;
+       end else begin
+           oled_data = 16'h0000;
+       end
+   end
+
 endmodule
